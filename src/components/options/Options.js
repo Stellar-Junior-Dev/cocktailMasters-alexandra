0955d63548580923eaf7dc744a4df45f62eb33eb
ioanalexandra <alexandra.ioan.99@gmail.com>
2023-05-04 14:49:44 +0300

    search + options html css

--- src/components/options/Options.js
+++ src/components/options/Options.js
@@ -1,32 +1,35 @@
 import "./options.css";
-import x from "../../img/x.svg"
-import copyright from "../../img/copyright.svg"
-export function Options(){
-    
-    return(
-            <div class="background">
-                <div class="overlay">
-                <div class="close">
-                    <img src={x}></img>
-                </div>
-                <div class="opt">
-                <h1>HOME</h1>
-                <h1>FAVOURITES</h1>
-                <h1>POPULAR DRINKS</h1>
-                <h1>LATEST DRINKS</h1>
-                <h1>RANDOM DRINKS</h1>
-                </div>
+import x from "../../img/x.svg";
+import copyright from "../../img/copyright.svg";
 
+export function Options(open) {
+  function hideOptions() {
+    const searchBackground = document.querySelector(".options-background");
+    searchBackground.classList.add("hide-options");
+  }
 
-                <div class="copyright">
-                    <div class="image">
-                    <img src={copyright}></img>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div class={`options-background ${open ? "hide-options" : ""}`}>
+        <div class="overlay">
+          <div class="close" onClick={hideOptions}>
+            <img src={x}></img>
+          </div>
+          <div class="opt">
+            <h1>HOME</h1>
+            <h1>FAVOURITES</h1>
+            <h1>POPULAR DRINKS</h1>
+            <h1>LATEST DRINKS</h1>
+            <h1>RANDOM DRINKS</h1>
+          </div>
 
-
-                </div>
+          <div class="copyright">
+            <div class="image">
+              <img src={copyright}></img>
             </div>
-
-    );
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
