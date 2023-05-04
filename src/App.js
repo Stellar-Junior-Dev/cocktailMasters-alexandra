7d4bd4658fb10cae170d1bfd9eefc205f0c7f485
ioanalexandra <alexandra.ioan.99@gmail.com>
2023-05-04 17:37:10 +0300

    show/hide cu state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import { Category } from "./components/category/Category";
-import { Options } from "./components/options/Options";
-import { Search } from "./components/search/Search";
 import { Controls } from "./components/controls/Controls";
 
 function App() {
   return (
     <div>
-      <Search open={false} />
-      <Options open={false} />
       <Controls />
       <div class="title">
         <h2>COCKTAIL MASTER</h2>
--- src/components/options/Options.js
+++ src/components/options/Options.js
@@ -2,31 +2,29 @@ import "./options.css";
 import x from "../../img/x.svg";
 import copyright from "../../img/copyright.svg";
 
-export function Options(open) {
-  function hideOptions() {
-    const searchBackground = document.querySelector(".options-background");
-    searchBackground.classList.add("hide-options");
-  }
-
+export function Options({ open, toggleOpen }) {
   return (
-    <div>
-      <div class={`options-background ${open ? "hide-options" : ""}`}>
-        <div class="overlay">
-          <div class="close" onClick={hideOptions}>
-            <img src={x} alt="Close icon" />
-          </div>
-          <div class="opt">
-            <h1>HOME</h1>
-            <h1>FAVOURITES</h1>
-            <h1>POPULAR DRINKS</h1>
-            <h1>LATEST DRINKS</h1>
-            <h1>RANDOM DRINKS</h1>
-          </div>
+    <div class={`options-background ${!open ? "hide-options" : ""}`}>
+      <div class="overlay">
+        <div
+          class="close"
+          onClick={() => {
+            toggleOpen(false);
+          }}
+        >
+          <img src={x} alt="Close icon" />
+        </div>
+        <div className="opt">
+          <h1>HOME</h1>
+          <h1>FAVOURITES</h1>
+          <h1>POPULAR DRINKS</h1>
+          <h1>LATEST DRINKS</h1>
+          <h1>RANDOM DRINKS</h1>
+        </div>
 
-          <div class="copyright">
-            <div class="copyright-image">
-              <img src={copyright} alt="Copyright details" />
-            </div>
+        <div class="copyright">
+          <div class="copyright-image">
+            <img src={copyright} alt="Copyright details" />
           </div>
         </div>
       </div>
