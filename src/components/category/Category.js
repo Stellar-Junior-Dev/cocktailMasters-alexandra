05243d325fa96d488c4ee260dc3e2d0fd407bdb1
ioanalexandra <alexandra.ioan.99@gmail.com>
2023-05-15 12:29:26 +0300

    bug fic

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -2,7 +2,7 @@ import "./category.css";
 import { Card } from "../card/Card";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-export function Category({ categoryTitle, cocktails, id, className }) {
+export function Category({ categoryTitle, cocktails, id, className, source }) {
   const dispatch = useDispatch();
   return (
     <>
@@ -11,16 +11,18 @@ export function Category({ categoryTitle, cocktails, id, className }) {
           <div className="catTitle">
             <h1>{categoryTitle}</h1>
           </div>
-          <div className="view">
-            <Link
-              to={`/category/${id}`}
-              onClick={() => {
-                dispatch({ type: "SET_CATEGORY", payload: { id: id } });
-              }}
-            >
-              view all {">"}
-            </Link>
-          </div>
+          {source && (
+            <div className="view">
+              <Link
+                to={`/category/${id}`}
+                onClick={() => {
+                  dispatch({ type: "SET_CATEGORY", payload: { id: id } });
+                }}
+              >
+                view all {">"}
+              </Link>
+            </div>
+          )}
         </div>
 
         <div className={className ? className : "cardContainer"}>
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -20,6 +20,7 @@ export function HomePage() {
             categoryTitle={category.categoryTitle}
             cocktails={category.cocktails}
             key={category.id}
+            source={"homepage"}
           />
         ))}
       </div>
